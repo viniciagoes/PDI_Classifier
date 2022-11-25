4d319360349e5d6203dcaae6b4f545404215a185
viniciagoes <vialvesgoes@gmail.com>
2022-11-24 22:49:15 -0300

    a

---
+++ classifier.py
@@ -0,0 +1,10 @@
+import numpy as np
+import skimage as ski
+import cv
+import matplotlib.pyplot as plt
+
+#caminho da maquina do vinicius
+image = ski.io.imread('C:/Users/vialv/OneDrive/Documents/2022.2/PDI/PDI_Classifier/Picture/can_opener/0-01-V1-B.png')
+
+
+print(image)
